f89fe0e9d5346fbe1b8816a9eef0391d4a62d9af
Kartik Syal <kartik@rtesoftwares.com>
2023-03-11 13:01:08 +0530

    second commit

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ ddsmoothmenu.init({
                 <li><a href="index.html" class="selected"><span></span>Home Test 2 456</a></li>
                 <li><a href="about.html"><span></span>About Us</a>
                     <ul>
-                        <li><a href="#">Sub menu 1</a></li>
+                        <li><a href="#">Sub menu 1 test 1</a></li>
                         <li><a href="#">Sub menu 2</a></li>
                         <li><a href="#">Sub menu 3</a></li>					
                     </ul>
